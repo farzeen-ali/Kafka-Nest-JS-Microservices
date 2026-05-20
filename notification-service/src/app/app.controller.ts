f8895ef65db4ceab42b9f943f83c79b5ca33bd5c
farzeen-ali <farzeenaliarif786@gmail.com>
2026-05-20 22:32:54 +0500

    DLQ

--- notification-service/src/app/app.controller.ts
+++ notification-service/src/app/app.controller.ts
@@ -1,11 +1,35 @@
-import { Controller } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Controller, Inject, OnModuleInit } from "@nestjs/common";
+
+import { ClientKafka, EventPattern, Payload } from "@nestjs/microservices";
 
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit{
+  constructor(
+    @Inject('KAFKA_SERVICE')
+    private readonly kafkaClient : ClientKafka,
+  ) {};
+  async onModuleInit(){
+    await this.kafkaClient.connect();
+  }
   @EventPattern('user_created')
-  handleUserCreated(@Payload() data: any){
-    console.log('Event Recieved!')
+  async handleUserCreated(@Payload() data: any){
+    try {
+      console.log('Main Event Recieved!')
+      console.log(data);
+      throw new Error('Payment Service Failed!');
+    } catch (error: unknown) {
+      const errorMessage = error instanceof Error ? error.message : String(error);
+      console.log('Sending Event to DLQ');
+      this.kafkaClient.emit('user_created_dlq', {
+        failedData: data,
+        error: errorMessage,
+        failedAt: new Date()
+      })
+    }
+  }
+  @EventPattern('user_created_dlq')
+  handleDLQ(@Payload() data: any){
+    console.log('DLQ EVENT RECEIVED!')
     console.log(data)
   }
 }
--- notification-service/src/app/app.module.ts
+++ notification-service/src/app/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { AppController } from "./app.controller";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'notification-client',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'notification-consumer-group'
+          }
+        }
+      }
+    ])
+  ],
+  controllers: [AppController]
 })
-export class AppModule {}
+export class AppModule{};
